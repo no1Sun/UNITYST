3fc1eaf73b1534c03b3db3c306cafa34726bc96a
tykim <tykim02@i-ops.co.kr>
2022-01-06 19:35:21 +0900

    first commit

--- InvenSystem/Inven.cs
+++ InvenSystem/Inven.cs
@@ -4,10 +4,27 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-//
+//인벤토리에 담을수 있는 최대 아이템 수 정해야 한다.
+//X*Y
 
 class Inven
 {
+    
+    Item[] ArrItem;
 
+    public Inven(int _X, int _Y)
+    {
+        //
+        if(_X < 1)
+        {
+            _X = 1;
+        }
+        if (_Y < 1)
+        {
+            _Y = 1;
+        }
+
+        ArrItem = new Item();
+    }
 }
 
